7825de786cf28ebae945b7b0d5aeeeb9f39ddb03
Harshit Mishra <harshitlove28@outlook.com>
2023-11-26 00:49:15 +0530

    Add Hero component and NavBarComp

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+  alias: {
+    "@components": "./components",
+    "@app": "./app",
   },
-  plugins: [],
-}
+  theme: {},
+  daisyui: {
+    themes: ["bumblebee"],
+  },
+  plugins: [require("daisyui")],
+};
